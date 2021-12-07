775f09daa124a24326451e140e67f5c285d84f11
Renaud LITTOLFF <rlittolff@gmail.com>
2021-12-07 22:42:15 +0100

    Day 2 / Puzzle 1

---
+++ day2/puzzle1.ts
@@ -0,0 +1,52 @@
+import * as readline from 'readline';
+import { createReadStream } from 'fs';
+import { Interface } from 'readline';
+
+class Puzzle {
+  public static main(filePath: string): void {
+    const puzzle = new Puzzle(filePath)
+    puzzle.run();
+  }
+
+  constructor(private filePath: string) {
+  }
+
+  async run(): Promise<void> {
+    const file = this.openFile();
+    console.log(`Result: ${await Puzzle.solve(file)}`);
+  }
+
+  private openFile(): Interface {
+    return readline.createInterface({
+      input: createReadStream(this.filePath),
+      crlfDelay: Infinity
+    });
+  }
+
+  private static async solve(file: Interface): Promise<number> {
+    let position = 0;
+    let depth = 0;
+
+    for await (const line of file) {
+      const [action, number] = line.split(' ');
+
+      switch (action) {
+        case 'forward':
+          position += parseInt(number);
+          break;
+
+        case 'up':
+          depth -= parseInt(number);
+          break;
+
+        case 'down':
+          depth += parseInt(number);
+          break;
+      }
+    }
+
+    return position * depth;
+  }
+}
+
+Puzzle.main(__dirname + '/data.txt');
