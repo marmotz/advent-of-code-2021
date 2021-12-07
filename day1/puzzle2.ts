8bacc360fbe00f885fc171fd01aeaa8200c1a2b5
Renaud LITTOLFF <rlittolff@gmail.com>
2021-12-07 22:27:17 +0100

    Day 1 / Puzzle 2

--- day1/puzzle2.ts
+++ day1/puzzle2.ts
@@ -24,17 +24,21 @@ class Puzzle {
   }
 
   private static async solve(file: Interface): Promise<number> {
-    let previous;
+    let previous1 = null;
+    let previous2 = null;
+    let previous3 = null;
     let count = 0;
 
     for await (const line of file) {
-      if (previous) {
-        if (parseInt(line) > previous) {
+      if (previous1 && previous2 && previous3) {
+        if (parseInt(line) + previous1 + previous2 > previous1 + previous2 + previous3) {
           count++;
         }
       }
 
-      previous = parseInt(line);
+      previous3 = previous2;
+      previous2 = previous1;
+      previous1 = parseInt(line);
     }
 
     return count;
